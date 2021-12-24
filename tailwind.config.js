464f86e3d655b491ec74a9654ef5becaf26da636
Gevorg-crypto <testerzone44@gmail.com>
2021-12-24 16:22:01 +0400

    lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,24 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
-      colors:{
-          green:{
-            800:"rgb(126, 211, 33)"
-          },
-        pink:{
-            800:'#e990f9'
+      colors: {
+        green: {
+          800: 'rgb(126, 211, 33)',
         },
-          gray:{
-              100:'rgb(234,234,234)',
-              500:'#b0b0b0',
-              800:'rgb(151,151,151)'
-          }
-      }
+        pink: {
+          800: '#e990f9',
+        },
+        gray: {
+          100: 'rgb(234,234,234)',
+          500: '#b0b0b0',
+          800: 'rgb(151,151,151)',
+        },
+      },
     },
   },
   plugins: [],
-}
+};
